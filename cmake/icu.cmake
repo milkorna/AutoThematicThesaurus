b48bc676043e8043cf9c2fd23253f80c9d5530f3
milkorna <milkornansk@gmail.com>
2024-04-14 22:19:18 +0700

    CMake draft configuration

---
+++ cmake/icu.cmake
@@ -0,0 +1,4 @@
+set (ICU_LIBRARIES icui18n icuuc icudata)
+set(XMorphy_SOURCE_DIR "${XMORPHY_PROJECT_PATH}")
+set (ICU_INCLUDE_DIR "${XMORPHY_PROJECT_PATH}/contrib/icu/icu4c/source/common")
+set (ICU_SOURCE_DIR "${XMORPHY_PROJECT_PATH}/contrib/icu/icu4c/source/common")
