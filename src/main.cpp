3e78e8ca640b48dd6d20a76f02ddd458d2d07bda
milkorna <milkornansk@gmail.com>
2024-08-08 22:30:46 +0700

    Added semantic relations database from wikiwordnet

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,16 @@
 #include <OutputRedirector.h>
 #include <PatternPhrasesStorage.h>
 
+#include <SemanticRelations.h>
+
 #include <chrono>
 #include <filesystem>
 
 namespace fs = std::filesystem;
+
+void printFirstFiveRows(const std::string& table_name)
+{
+}
 
 int main()
 {
@@ -16,6 +22,11 @@ int main()
     std::string logFilePath = (repoPath / "my_logs.txt").string();
     Logger::initializeLogFile(logFilePath);
 
+    //    std::string semantic_data = (repoPath / "wikiwordnet.db").string();
+    //    SemanticRelationsDB db(semantic_data);
+
+    DB::RunTest();
+
     fs::path patternsPath = repoPath / "my_data/patterns.txt";
     GrammarPatternManager::GetManager()->readPatterns(patternsPath);
 
@@ -24,7 +35,7 @@ int main()
     auto end = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double> duration = end - start;
-    Logger::log("\n\nmain", LogLevel::Info, "Processing texts took " + std::to_string(duration.count()) + " seconds.");
+    Logger::log("\n\nmain", LogLevel::Info, "Processing texts took " + std::to_string(duration.count()) + "seconds.");
 
     return 0;
 }
